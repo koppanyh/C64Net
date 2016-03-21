ca5c29714a41ea88f2d9e784b90e37e538e11594
Koppany Horvath <koppanyh@users.noreply.github.com>
2016-03-21 09:24:23 -0700

    Added date to file.

--- tapwriter.py
+++ tapwriter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #Created by Koppany Horvath
+#Date 2016-03-10
 cmds = ["END","FOR","NEXT","DATA","INPUT#","INPUT","DIM","READ","LET","GOTO","RUN","IF","RESTORE","GOSUB","RETURN","REM","STOP","ON","WAIT","LOAD","SAVE","VERIFY","DEF","POKE","PRINT#","PRINT","CONT","LIST","CLR","CMD","SYS","OPEN","CLOSE","GET","NEW","TAB(","TO","FN","SPC(","THEN","NOT","STEP","+","-","*","/","^","AND","OR",">","=","<","SGN","INT","ABS","USR","FRE","POS","SQR","RND","LOG","EXP","COS","SIN","TAN","ATN","PEEK","LEN","STR$","VAL","ASC","CHR$","LEFT$","RIGHT$","MID$","GO"] #list of basic commands
 #cmds 128 - 203
 
